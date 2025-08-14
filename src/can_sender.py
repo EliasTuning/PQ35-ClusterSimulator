c599277bd31ae2fe255b6f302647687c0c6c99b9
Elias Kotlyar <elias.kotlyar@gmail.com>
2025-08-14 21:02:25 +0200

    CAN working

--- src/can_sender.py
+++ src/can_sender.py
@@ -1,8 +1,10 @@
 class CanSender:
     def __init__(self):
+        #bus = can.interface.Bus(channel='can0', bustype='socketcan')
         pass
 
     def send(self, message_func):
         message = message_func()
         print(message)
+        #bus.send(can_message)
         pass
